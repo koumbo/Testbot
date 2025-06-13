8e2454962b74fa28fb07d4ee1d1ca26f1fa3a48f
koumbo <113204557+koumbo@users.noreply.github.com>
2025-06-13 11:16:15 +0000

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -1,4 +1,4 @@
-#import libraries
+# Import libraries
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 
@@ -22,18 +22,17 @@ def run_bot(user_text, chat_history_ids):
     # Tokenize the user's text and append the end-of-sequence token
     input_ids = tokenizer.encode(user_text + tokenizer.eos_token, return_tensors="pt")
     
-     # Concatenate the chat history with the user's input, if available
+    # Concatenate the chat history with the user's input, if available
     if chat_history_ids is None:
         bot_input_ids = input_ids
     else:
         bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1)
         
     # Generate a response from the model based on the concatenated input
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000,pad_token_id=tokenizer.eos_token_id)
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
     
     # Decode the generated response and remove special tokens for display
-    resp = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0],skip_special_tokens=True)
+    resp = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
     
     # Return the decoded response and updated chat history
     return resp, chat_history_ids
-
